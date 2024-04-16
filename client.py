1a26d188df33d4daa147258ed65b8405891ff93f
arKy4n <rkmvaryan@gmail.com>
2024-04-16 17:35:02 +0900

    Simple group chat

--- client.py
+++ client.py
@@ -1,11 +1,42 @@
 import socket
+import threading
+import CONSTANTS
 
-HOST = "192.168.11.7"
-PORT = 8080
-FORMAT = "utf-8"
+# Choosing NICKNAME
+nickname = input("Choose your NICKNAME: ")
 
+# Connecting to Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+client.connect(CONSTANTS.ADDR)
 
-client.send("Hello WOrld!".encode(FORMAT))
-print(client.recv(1024).decode(FORMAT))
+def receive():
+    CONNECTED = True
+    while CONNECTED:
+        try: 
+            message = client.recv(1024).decode(CONSTANTS.FORMAT)
+            if message == "NICKNAME":
+                client.send(nickname.encode(CONSTANTS.FORMAT))
+            else:
+                print(message)
+        except Exception as e:
+            print(f"Some error occured during the session: {e}")
+            client.close()
+            CONNECTED = False
+
+def write():
+    CONNECTED = True
+    while CONNECTED:
+        try:
+            message = f"{nickname}: {input("")}"
+            client.send(message.encode(CONSTANTS.FORMAT))
+        except Exception as e:
+            print(f"Some error occured during the session: {e}")
+            client.close()
+            CONNECTED = False
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -1,20 +1,69 @@
 import socket
+import threading
+import CONSTANTS
 
-HOST = socket.gethostbyname(socket.gethostname())  # '192.168.11.7'
-PORT = 8080
-FORMAT = "utf-8"
-
+# Starting the Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-
+server.bind(CONSTANTS.ADDR)
 server.listen()
-# server.listen(5) -> Maximum 5 connections are allowed
 
-while True:
-    communication_socket, address = server.accept()
-    print(f"Connected to {address}")
-    message = communication_socket.recv(1024).decode(FORMAT)
-    print(f"Message from client is: {message}")
-    communication_socket.send("Got your message! Thank you!".encode(FORMAT))
-    communication_socket.close()
-    print(f"Connection with {address} ended!")
+# Dictionary For Clients and Their Nicknames
+clients = {}
+
+
+# Sending Messages To All Other Connected Clients
+def broadcast(message, client=None):
+    for client_socket in list(clients.keys()):
+        if client_socket != client:
+            try:
+                client_socket.send(message)
+            except Exception as e:
+                # Removing and Closing Clients
+                print(f"Error sending message to client [{client_socket}]: {e}")
+                client_socket.close()
+                del clients[client_socket]
+
+
+# Handling Messages From the Clients
+def handle_client(client):
+    CONNECTED = True
+
+    while CONNECTED:
+        try:
+            # message = client.recv(1024).decode(CONSTANTS.FORMAT)
+            message = client.recv(1024)
+            broadcast(message, client)
+        except Exception as e:
+            # Removing and Closing Clients
+            print(f"Error handling client [{client}: {e}]")
+            client.close()
+            broadcast(f"Client [{client}] has diconnected".encode(CONSTANTS.FORMAT))
+            del clients[client]
+            CONNECTED = False
+
+
+# Receiving/Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        conn_socket, address = server.accept()
+        # print(f"Connected to {address}")
+
+        # Request and Store NICKNAMES
+        conn_socket.send("NICKNAME".encode(CONSTANTS.FORMAT))
+        nickname = conn_socket.recv(1024).decode(CONSTANTS.FORMAT)
+        clients[conn_socket] = nickname
+
+        # Broadcast NICKNAME in the Chat
+        print(f"{nickname} has connected to server")
+        broadcast(
+            f"{nickname} has joined the chat".encode(CONSTANTS.FORMAT), conn_socket
+        )
+        conn_socket.send("Connected to server!".encode(CONSTANTS.FORMAT))
+
+        # Start Handling Thread For the Client
+        thread = threading.Thread(target=handle_client, args=(conn_socket,))
+        thread.start()
+
+
+receive()
